1b4e8353403f542e533bcb80517b5ee95785e3d1
krepowo <krep.contact@proton.me>
2025-07-19 09:41:30 +0700

    Change Ryzumi API baseUrl

--- src/utils/ryzumi.js
+++ src/utils/ryzumi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export async function fetchRyzumiAPI(path, parameter = {}) {
-    const baseUrl = 'https://api.ryzumi.vip/api';
+    const baseUrl = 'https://apidl.asepharyana.tech/api';
     try {
         const response = await axios.get(`${baseUrl}${path}`, {
             params: parameter,
